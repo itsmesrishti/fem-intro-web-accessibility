3e9837afafb332dfc904cd8499957a46c91cbd42
itsmesrishti <srishticho@gmail.com>
2025-08-21 22:06:54 +0530

    fix js code

--- interactive-rating-component-main/script.js
+++ interactive-rating-component-main/script.js
@@ -1,7 +1,50 @@
 const ratingForm = document.getElementById("rating-form");
 const submitBtn = document.getElementById("submit-btn");
+const inputFields = Array.from(document.querySelectorAll("fieldset input"));
+const thanksCard = document.getElementById("thanks-card");
+const selectedRating = document.getElementById("selected-rating");
+
+function findRating(hasRating) {
+  let rating = undefined;
+
+  switch (hasRating) {
+    case "one-star":
+      rating = 1;
+      break;
+    case "two-star":
+      rating = 2;
+      break;
+    case "three-star":
+      rating = 3;
+      break;
+    case "four-star":
+      rating = 4;
+      break;
+    case "five-star":
+      rating = 5;
+      break;
+  }
+
+  return rating;
+}
+
+function isRatingGiven() {
+  let hasRating = inputFields.filter((input) => input.checked);
+  if (hasRating.length === 0) {
+    return false;
+  } else {
+    selectedRating.innerText = findRating(hasRating[0]["id"]);
+    return hasRating;
+  }
+}
 
 submitBtn.addEventListener("click", (e) => {
-    e.preventDefault();
-    window.alert("clicked submit btn");
+  e.preventDefault();
+
+  if (isRatingGiven()) {
+    thanksCard.removeAttribute("hidden");
+    ratingForm.setAttribute("hidden", true);
+  } else {
+    window.alert("give a rating first");
+  }
 });
