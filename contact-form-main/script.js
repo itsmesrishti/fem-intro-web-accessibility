d4e86b1e3bed635d9daec3df07a98ce59db945ce
itsmesrishti <srishticho@gmail.com>
2025-11-12 18:35:22 +0530

    update js

--- contact-form-main/script.js
+++ contact-form-main/script.js
@@ -5,24 +5,26 @@ const email = document.getElementById("email");
 const fNameError = document.getElementById("fName-instructions");
 const lNameError = document.getElementById("lName-instructions");
 const emailError = document.getElementById("email-instructions");
+const inputs = document.querySelectorAll("input");
+const errorMsgs = document.querySelectorAll(".text-error")
+
+function checkInput(inputs) {
+    inputs.forEach((input) => {
+        if (input.value === "" || input.checked === false) {
+            errorMsgs.forEach((errorMsg) => {
+                if (errorMsg.id = input.getAttribute("aria-describedby")) {
+                    errorMsg.hidden = false;
+                    input.classList.add("error-outline")
+                }
+                
+            })
+        } else {
+            
+        }
+    })
+}
 
 submitBtn.addEventListener("click", (e) => {
-    e.preventDefault();
-
-    if (fName.value === "" || lName.value === "" || email.value === "") {
-        fNameError.hidden = false;
-        fName.classList.add("error-outline");
-        lNameError.hidden = false;
-        lName.classList.add("error-outline");
-        emailError.hidden = false;
-        email.classList.add("error-outline");
-    }
-    else {
-        fNameError.hidden = true;
-        fName.classList.remove("error-outline");
-        lNameError.hidden = true;
-        lName.classList.remove("error-outline");
-        emailError.hidden = true;
-        email.classList.remove("error-outline");
-    }
-})
+  e.preventDefault();
+  checkInput(inputs);
+});
