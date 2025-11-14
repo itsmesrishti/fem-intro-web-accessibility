f6749775b495a8ea7c88e0ce079bb1495757e3f6
itsmesrishti <srishticho@gmail.com>
2025-11-14 09:42:48 +0530

    fix logic of checking filled unfilled input

--- contact-form-main/script.js
+++ contact-form-main/script.js
@@ -6,25 +6,58 @@ const fNameError = document.getElementById("fName-instructions");
 const lNameError = document.getElementById("lName-instructions");
 const emailError = document.getElementById("email-instructions");
 const inputs = document.querySelectorAll("input");
-const errorMsgs = document.querySelectorAll(".text-error")
+const errorMsgs = document.querySelectorAll(".text-error");
 
-function checkInput(inputs) {
-    inputs.forEach((input) => {
-        if (input.value === "" || input.checked === false) {
-            errorMsgs.forEach((errorMsg) => {
-                if (errorMsg.id = input.getAttribute("aria-describedby")) {
-                    errorMsg.hidden = false;
-                    input.classList.add("error-outline")
-                }
-                
-            })
-        } else {
-            
-        }
-    })
+let emptyInputs = [];
+let nonEmptyInputs = [];
+
+function separateInputs(inputs) {
+  inputs.forEach((input) => {
+    if (input.type === "radio" || input.type === "checkbox") {
+      if (input.checked === false) {
+        emptyInputs.push(input.id);
+      } else {
+        nonEmptyInputs.push(input.id);
+      }
+    } else if (input.type === "text" || input.type === "email") {
+      if (input.value === "") {
+        emptyInputs.push(input.id);
+      } else {
+        nonEmptyInputs.push(input.id);
+      }
+    }
+  });
+}
+
+function showError() {
+  emptyInputs.forEach((input) => {
+    errorMsgs.forEach((errorMsg) => {
+      if ((errorMsg.id = input.getAttribute("aria-describedby"))) {
+        errorMsg.hidden = false;
+        input.classList.add("error-outline");
+      }
+    });
+  });
+}
+
+function removeError() {
+  nonEmptyInputs.forEach((input) => {
+    errorMsgs.forEach((errorMsg) => {
+      if ((errorMsg.id = input.getAttribute("aria-describedby"))) {
+        errorMsg.hidden = true;
+        input.classList.remove("error-outline");
+      }
+    });
+  });
 }
 
 submitBtn.addEventListener("click", (e) => {
   e.preventDefault();
-  checkInput(inputs);
+  console.log("empty: " + emptyInputs);
+  console.log("non-empty: " + nonEmptyInputs);
+  separateInputs(inputs);
+  console.log("empty: " + emptyInputs);
+  console.log("non-empty: " + nonEmptyInputs);
+  //   showError();
+  //   removeError();
 });
